6f3f4eec4a14e01484222126fec6809952586c32
barit <mmyers.com@Gmail.com>
2023-09-19 12:04:54 -0500

    complete lab

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onRemovePlant }) {
+  const [isInStock, setIsInStock] = useState(true);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleRemove = () => {
+    onRemovePlant(plant.id);
+  };
+
+  const toggleStock = () => {
+    setIsInStock(!isInStock);
+  };
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      <button className="primary" onClick={toggleStock}>
+        In Stock
+      </button>
+      {!isInStock && (
+        <button
+          onClick={handleRemove}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {isHovered ? "Delete?" : "Out of Stock"}
+        </button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+  const handleRemovePlant = (plantId) => {
+    fetch(`http://localhost:6001/plants/${plantId}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.ok) {
+          setPlants((prevPlants) =>
+            prevPlants.filter((plant) => plant.id !== plantId)
+          );
+        } else {
+          throw new Error("You cant delete this fool");
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  const addPlant = (newPlant) => {
+    setPlants([...plants, newPlant]);
+  };
+
+  const handleSearchChange = (term) => {
+    setSearchTerm(term);
+  };
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={addPlant} />
+      <Search onSearchChange={handleSearchChange} />
+      <PlantList plants={filteredPlants} onRemovePlant={handleRemovePlant} />
     </main>
   );
 }
